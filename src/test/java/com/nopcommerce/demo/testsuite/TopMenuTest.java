b0efb74256c6665ff89aa448f81ac396726f1d68
sweetromoon <sweetromoon@gmail.com>
2023-05-01 12:28:49 +0100

    My homework wk13

---
+++ src/test/java/com/nopcommerce/demo/testsuite/TopMenuTest.java
@@ -0,0 +1,34 @@
+package com.nopcommerce.demo.testsuite;
+
+import com.nopcommerce.demo.pages.HomePage;
+import com.nopcommerce.demo.testbase.BaseTest;
+import org.testng.annotations.Test;
+
+public class TopMenuTest extends BaseTest {
+
+    HomePage homePage = new HomePage();
+
+    @Test
+    public void verifyPageNavigation() {
+        homePage.selectMenu("computer");
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
